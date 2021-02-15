51ba541bac0a2fe12ada5fb94f3c1dbb50084895
shray0027 <59202921+shray0027@users.noreply.github.com>
2021-02-15 15:34:13 +0530

    update qoute

--- public/js/qoute.js
+++ public/js/qoute.js
@@ -1,6 +1,6 @@
 const content = document.querySelector(".qoutes-content");
 const author = document.querySelector(".qoutes-author");
-fetch('http://conference-jam.herokuapp.com/qoutes').then((res,error)=>{
+fetch('https://conference-jam.herokuapp.com/qoutes').then((res,error)=>{
     res.json().then((data)=> {
         console.log(data);
             content.textContent='" '+data.content+' "';
