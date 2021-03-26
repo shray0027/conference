639f8091af90ab00bd2f0c6ab9f9076133723ef7
shray0027 <59202921+shray0027@users.noreply.github.com>
2021-03-26 15:52:32 +0530

    conference-page

--- public/js/toolbar.js
+++ public/js/toolbar.js
@@ -1,85 +1,34 @@
-const videoBtn = document.querySelector(".video_on");
-const micBtn = document.querySelector(".mic_on");
-const chatBtn = document.querySelector(".chat_on");
-const infoBtn= document.querySelector(".info_on");
-const participantBtn= document.querySelector(".participants_on");
-const settingBtn= document.querySelector(".settings_on");
-const settingIcon=document.querySelector(".fa-cog");
-let videoOn=true; 
-const closeVideo=()=>{
-    if(videoOn){
-        videoBtn.innerHTML="<i class='fas fa-video-slash'></i>";
-        videoBtn.classList.add("off");
-        videoBtn.classList.remove("on");
-        videoOn=false;
-    } else{
-        videoBtn.innerHTML="<i class='fas fa-video'></i>";
-        videoBtn.classList.add("on");
-        videoBtn.classList.remove("off");
-        videoOn=true;
-    }
-}
-let micOn=true;
-const closeMic=()=>{
-    if(micOn){
-        micBtn.innerHTML="<i class='fas fa-microphone-slash'></i>";
-        micBtn.classList.add("off");
-        micBtn.classList.remove("on");
-        micOn=false;
+const chat = document.querySelector(".chat");
+const mic = document.querySelector(".mic");
+const video = document.querySelector(".video");
+const mainRight = document.querySelector(".main_right").style;
+let toggleChat=true;
+chat.addEventListener("click",()=>{
+    if(toggleChat){
+        mainRight.display="block";
+        toggleChat=false;
     } else {
-        micBtn.innerHTML='<i class="fas fa-microphone"></i>';
-        micBtn.classList.add("on");
-        micBtn.classList.remove("off");
-        micOn=true;
+        mainRight.display="none";
+        toggleChat=true;
     }
-}
-let chatOn=true;
-const closeMsg=()=>{
-    if(chatOn){
-        chatBtn.classList.add("offBlue");
-        chatBtn.classList.remove("on");
-        chatOn=false;
+})
+let toggleMic=true;
+mic.addEventListener("click",()=>{
+    if(toggleMic){
+        mic.innerHTML='<i class="fas fa-microphone"></i>';
+        toggleMic=false;
     } else {
-        chatBtn.classList.add("on");
-        chatBtn.classList.remove("offBlue");
-        chatOn=true;
+        mic.innerHTML='<i class="fas fa-microphone-slash"></i>';
+        toggleMic=true;
     }
-}
-let infoOn=true;
-const showInfo=()=>{
-    if(infoOn){
-        infoBtn.classList.add("offBlue");
-        infoBtn.classList.remove("on");
-        infoOn=false;
+})
+let toggleVideo=true;
+video.addEventListener("click",()=>{
+    if(toggleVideo){
+        video.innerHTML='<i class="fas fa-video"></i>';
+        toggleVideo=false;
     } else {
-        infoBtn.classList.add("on");
-        infoBtn.classList.remove("offBlue");
-        infoOn=true;
+        video.innerHTML='<i class="fas fa-video-slash"></i>';
+        toggleVideo=true;
     }
-}
-let settingOn=true;
-const showSettings=()=>{
-    if(settingOn){
-        settingBtn.classList.add("offGreen");
-        settingBtn.classList.remove("on");
-        settingIcon.classList.add("rotate");
-        settingOn=false;
-    } else {
-        settingBtn.classList.add("on");
-        settingBtn.classList.remove("offGreen");
-        settingIcon.classList.remove("rotate");
-        settingOn=true;
-    }
-}
-let participantOn=true;
-const showParticipants=()=>{
-    if(participantOn){
-        participantBtn.classList.add("offGreen");
-        participantBtn.classList.remove("on");
-        participantOn=false;
-    } else {
-        participantBtn.classList.add("on");
-        participantBtn.classList.remove("offGreen");
-        participantOn=true;
-    }
-}
+})
