101987fee62037ffeaa9ddcc9505a8552563f301
shray0027 <59202921+shray0027@users.noreply.github.com>
2021-04-06 16:06:44 +0530

    added google strategy

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/options"
+    callbackURL: "https://conference-jam.herokuapp.com/auth/google/options"
   },
   function(accessToken, refreshToken, profile, done) {
        User.findOrCreate({ name:profile.displayName,googleId: profile.id }, function (err, user) {
